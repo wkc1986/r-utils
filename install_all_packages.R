333d795d37f717868d57a7baf0dbcbfa6af8e5fd
William K Chang <wkc@nobook-heir-2.local>
2017-09-01 00:46:21 -0400

    package refresh script

---
+++ install_all_packages.R
@@ -0,0 +1,14 @@
+install.packages(c("tidyverse",
+									 "data.table",
+									 "devtools",
+									 "igraph",
+									 "ggraph",
+									 "cowplot",
+									 "philentropy",
+									 "infotheo",
+									 "entropy",
+									 "ggrepel",
+									 "Matrix",
+									 "imputeTS"),
+								repos = "http://cran.r-project.org")
+devtools::install_github("wkc1986/infograf")
