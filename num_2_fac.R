8b8cf388a7dd957053d52693de502bf88c5ccffd
wkc <williamkurtischang@gmail.com>
2018-08-27 14:54:41 -0400

    made a package

--- num_2_fac.R
+++ num_2_fac.R
@@ -1,5 +1,14 @@
+#' Numeric to factor
+#'
+#' @param v numeric (likely integer) vector
+#' @param ... arguments to `sort`
+#'
+#' @return factor vector
+#' @export
+#'
+#' @examples
 num_2_fac <- function(v, ...) {
   uv <- unique(v)
   factor(as.character(v),
-         levels = as.character(sort(uv), ...))
+         levels = as.character(sort(uv, ...)))
 }
